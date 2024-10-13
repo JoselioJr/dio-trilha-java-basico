e73a32ef04510fdfe371635fad9811a3ad3bfabc
joseliojr <joselio.fc@hotmail.com>
2024-10-13 14:49:55 -0300

    Uptade Código do projeto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hellow World");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da Conta: ");
+        int numeroConta = scan.nextInt();
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scan.next();
+
+        System.out.print("Por favor, digite o seu nome: ");
+        String nomeCliente = scan.next();
+
+        System.out.print("Por favor, digite o seu saldo: ");
+        double saldo = scan.nextDouble();
+
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numeroConta+" e seu saldo R$"+saldo+" já está disponível para saque.");
     }
 }
